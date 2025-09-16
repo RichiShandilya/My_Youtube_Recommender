6e4c071acac87eaa28b030de4a7a58d4337149a0
RichiShandilya <shandilyarichi@gmail.com>
2025-09-16 16:43:39 +0530

    Update Rec_Sys with env support and new API key handling

--- Rec_Sys.py
+++ Rec_Sys.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
+import os
 import requests
 import pandas as pd
 from googleapiclient.discovery import build
 
-API_KEY = "AIzaSyA91PXIIZNTKgY9YOqYW6oR8O4AiS9IRpw"
+load_dotenv()
+API_KEY = os.getenv("YT_API_KEY")
 youtube = build("youtube", "v3", developerKey=API_KEY)
 
 
